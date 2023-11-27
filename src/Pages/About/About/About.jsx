5ee5f27477c7718971bf70e64fddde9914474972
Gabriel Parada <gabrielparada05@gmail.com>
2023-11-27 15:00:18 -0500

    bug fixed rendering about

--- src/Pages/About/About/About.jsx
+++ src/Pages/About/About/About.jsx
@@ -5,7 +5,7 @@ import Appoinment from '../../Home/Appoinment/Appoinment';
 import Content from '../AboutContent/Content';
 import Achivement from '../Achivement/Achivement';
 import Banner from '../Banner/Banner';
-import Dentist from '../ExpertDentist/Dentist';
+
 import ScrollButton from '../../../components/ScrollBtn/ScrollBtn';
 
 const About = () => {
@@ -14,12 +14,12 @@ const About = () => {
           <Banner />
           <ScrollButton/>
           <Content /> 
-            {/* <AboutService />
-            <Dentist />
+              {/* <AboutService />
+         
             <Achivement /> */}
           <Appoinment />
         </>
-    );
+      );
 };
 
 export default About;
