104d700b3295e721730abea7cd0ca4c49cd092c2
Gabriel Parada <gabrielparada05@gmail.com>
2023-12-02 21:10:39 -0500

    bug fixed app route adding hasroute

--- src/App.js
+++ src/App.js
@@ -13,59 +13,59 @@ import NotFound from './Pages/NotFound/NotFound';
 import Service from './Pages/Services/Service/Service';
 import Gallery from './Pages/Home/Gallery/Gallery.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <AuthProvider> */}
-      <Router basename="/proyar">
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/proyar' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/service' element={<Service />} />
-          <Route path='/gallery' element={<Gallery />} />
-          <Route path='/team' element={<Team />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/approved' element={<Approved />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
-    {/* </AuthProvider> */}
-    </div>
-  );
-}
-
-export default App;
-
-
 // function App() {
-//   const publicUrl = process.env.PUBLIC_URL;
-
 //   return (
 //     <div className="App">
-//       <AuthProvider>
-//         <Router>
-//           <Header />
-//           <Routes>
-//             <Route path={`${publicUrl}/`} element={<Home />} />
-//             <Route path={`${publicUrl}/proyar`} element={<Home />} />
-//             <Route path={`${publicUrl}/about`} element={<About />} />
-//             <Route path={`${publicUrl}/service`} element={<Service />} />
-//             <Route path={`${publicUrl}/team`} element={<Team />} />
-//             <Route path={`${publicUrl}/contact`} element={<Contact />} />
-//             <Route path={`${publicUrl}/approved`} element={<Approved />} />
-//             <Route path={`${publicUrl}/login`} element={<Login />} />
-//             <Route path={`${publicUrl}/*`} element={<NotFound />} />
-//           </Routes>
-//           <Footer />
-//         </Router>
-//       </AuthProvider>
+//       {/* <AuthProvider> */}
+//       <Router basename="/proyar">
+//         <Header />
+//         <Routes>
+//           <Route path='/' element={<Home />} />
+//           <Route path='/proyar' element={<Home />} />
+//           <Route path='/home' element={<Home />} />
+//           <Route path='/about' element={<About />} />
+//           <Route path='/service' element={<Service />} />
+//           <Route path='/gallery' element={<Gallery />} />
+//           <Route path='/team' element={<Team />} />
+//           <Route path='/contact' element={<Contact />} />
+//           <Route path='/approved' element={<Approved />} />
+//           <Route path='/login' element={<Login />} />
+//           <Route path='*' element={<NotFound />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     {/* </AuthProvider> */}
 //     </div>
 //   );
 // }
 
 // export default App;
+
+
+function App() {
+  const publicUrl = process.env.PUBLIC_URL;
+
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path={`${publicUrl}/`} element={<Home />} />
+            <Route path={`${publicUrl}/proyar`} element={<Home />} />
+            <Route path={`${publicUrl}/about`} element={<About />} />
+            <Route path={`${publicUrl}/service`} element={<Service />} />
+            <Route path={`${publicUrl}/team`} element={<Team />} />
+            <Route path={`${publicUrl}/contact`} element={<Contact />} />
+            <Route path={`${publicUrl}/approved`} element={<Approved />} />
+            <Route path={`${publicUrl}/login`} element={<Login />} />
+            <Route path={`${publicUrl}/*`} element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
