ebf1ae681d72ca9f1a1d80573963600061abf8f9
Gabriel Parada <gabrielparada05@gmail.com>
2023-12-02 19:49:01 -0500

    fixed bug deploy

--- src/App.js
+++ src/App.js
@@ -12,58 +12,58 @@ import Login from './Pages/Login/Login';
 import NotFound from './Pages/NotFound/NotFound';
 import Service from './Pages/Services/Service/Service';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <AuthProvider>
-//       <Router>
-//         <Header />
-//         <Routes>
-//           <Route path='/' element={<Home />} />
-//           <Route path='/proyar' element={<Home />} />
-//           <Route path='/home' element={<Home />} />
-//           <Route path='/about' element={<About />} />
-//           <Route path='/service' element={<Service />} />
-//           <Route path='/team' element={<Team />} />
-//           <Route path='/contact' element={<Contact />} />
-//           <Route path='/approved' element={<Approved />} />
-//           <Route path='/login' element={<Login />} />
-//           <Route path='*' element={<NotFound />} />
-//         </Routes>
-//         <Footer />
-//       </Router>
-//     </AuthProvider>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 function App() {
-  const publicUrl = process.env.PUBLIC_URL;
-
   return (
     <div className="App">
       <AuthProvider>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path={`${publicUrl}/`} element={<Home />} />
-            <Route path={`${publicUrl}/proyar`} element={<Home />} />
-            <Route path={`${publicUrl}/about`} element={<About />} />
-            <Route path={`${publicUrl}/service`} element={<Service />} />
-            <Route path={`${publicUrl}/team`} element={<Team />} />
-            <Route path={`${publicUrl}/contact`} element={<Contact />} />
-            <Route path={`${publicUrl}/approved`} element={<Approved />} />
-            <Route path={`${publicUrl}/login`} element={<Login />} />
-            <Route path={`${publicUrl}/*`} element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </AuthProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/proyar' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/team' element={<Team />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/approved' element={<Approved />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
     </div>
   );
 }
 
 export default App;
+
+
+// function App() {
+//   const publicUrl = process.env.PUBLIC_URL;
+
+//   return (
+//     <div className="App">
+//       <AuthProvider>
+//         <Router>
+//           <Header />
+//           <Routes>
+//             <Route path={`${publicUrl}/`} element={<Home />} />
+//             <Route path={`${publicUrl}/proyar`} element={<Home />} />
+//             <Route path={`${publicUrl}/about`} element={<About />} />
+//             <Route path={`${publicUrl}/service`} element={<Service />} />
+//             <Route path={`${publicUrl}/team`} element={<Team />} />
+//             <Route path={`${publicUrl}/contact`} element={<Contact />} />
+//             <Route path={`${publicUrl}/approved`} element={<Approved />} />
+//             <Route path={`${publicUrl}/login`} element={<Login />} />
+//             <Route path={`${publicUrl}/*`} element={<NotFound />} />
+//           </Routes>
+//           <Footer />
+//         </Router>
+//       </AuthProvider>
+//     </div>
+//   );
+// }
+
+// export default App;
