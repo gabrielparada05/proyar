3cd695d0013b49778886d08a5c2f8549982373eb
Gabriel Parada <gabrielparada05@gmail.com>
2023-12-04 09:57:12 -0500

    modif path in app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       {/* <AuthProvider> */}
-      <Router basename="/proyar">
+      <Router>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
