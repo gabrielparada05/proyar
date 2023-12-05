e8e7b7e6eb7c1d1a39aab480945551e48e14268d
Gabriel Parada <gabrielparada05@gmail.com>
2023-12-05 14:43:37 -0500

    style contact us

--- src/Pages/Contact/Contact/Contact.jsx
+++ src/Pages/Contact/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import Appointment from '../../../Pages/Home/Appoinment/Appoinment';
 import Banner from '../Banner/Banner';
 import ContactUs from '../ContactUs/ContactUs';
-import './ContactUs.css';
+import '../ContactUs/ContactUs.css';
 
 const Contact = () => {
 
